efd2d4a469a88d354babcc8c8b14c2d1f30ede71
Jenny Chang <jennychang@jennys-macbook-pro.local>
2019-01-18 22:08:30 -0800

    Initial Commit

---
+++ MultiplayerMathsTests/MultiplayerMathsTests.m
@@ -0,0 +1,37 @@
+//
+//  MultiplayerMathsTests.m
+//  MultiplayerMathsTests
+//
+//  Created by Jenny Chang on 18/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MultiplayerMathsTests : XCTestCase
+
+@end
+
+@implementation MultiplayerMathsTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
